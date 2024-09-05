fce8dbefd550e068864d0816125bc7c13b808c7e
Jan Reinhard <jan.reinhard@rwth-aachen.de>
2024-09-05 10:44:52 +0200

    add wither to orderstatus

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,9 +1,11 @@
+import lombok.With;
+
 import java.util.List;
 
 public record Order(
         String id,
         List<Product> products,
-        OrderStatus status
+        @With OrderStatus status
 ) {
     public Order(String id, List<Product> products){
         this(id,products,OrderStatus.PROCESSING);
